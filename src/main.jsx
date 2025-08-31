6d0a38b40854759a982dab5e6172b2d03b5b0e37
warfather0991 <nakib1990saleh@gmail.com>
2025-08-31 23:57:43 +0600

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import PublicRoutes from "./Routes/publicRoutes";
+import PublicRoutes from "./Routes/PublicRoutes";
 import AuthProvider from "./Provider/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
